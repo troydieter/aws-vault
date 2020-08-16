64db8fd9f6526fc42c0b6168cb98f72026956679
Troy Dieter <troydieter@gmail.com>
2020-08-16 14:38:09 -0400

    fixup

--- network/variables.tf
+++ network/variables.tf
@@ -2,37 +2,37 @@
 
 variable "aws_account" {
   description = "AWS Account Name"
-  type  = "string"
+  type  = string
 }
 variable "environment" {
   description = "AWS Environment"
-  type  = "string"
+  type  = string
 }
 
 
 
 variable "vpc_cidr" {
   description = "VPC CIDR"
-  type  = "string"
+  type  = string
 }
 
 
 variable "private_subnet_cidrs" {
 description = "Private subnet  - CIDR"
-type  = "list"
+type  = list
 }
 variable "public_subnet_cidrs" {
 description = "Private subnet  - CIDR"
-type  = "list"
+type  = list
 }
 
 
 variable "aws-profile" {
   description = "Local AWS Profile Name "
-  type  = "string"
+  type  = string
 }
 variable "aws_region" {
   description = "aws region"
   default="us-east-1"
-  type  = "string"
+  type  = string
 }
